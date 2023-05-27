a51b3130763ac49c38ba9c692e3ead5ccd496321
DanielKirshner <danielkirshner7878@gmail.com>
2023-05-27 14:13:57 +0200

    ctor is now explicit, and class is final

--- src/MySharedPtr.hpp
+++ src/MySharedPtr.hpp
@@ -1,9 +1,9 @@
-class MySharedPtr
+class MySharedPtr final
 {
     private:
         int* ptr;
         size_t* ref_count;
     
     public:
-        MySharedPtr();
+        explicit MySharedPtr();
 };
