b5451ca451aef4277bb73ddd013fd02768758884
Matan Brightbert <matana0000@gmail.com>
2023-05-27 21:29:40 +0300

    Improved the testing

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,29 @@
 
 int main()
 {
-    MyUniquePtr ptr1; // default c'tor
-    
     int* x = new int(5);
     std::cout << *x << std::endl;
-    
-
     {
         MyUniquePtr ptr4(x); 
         std::cout << *ptr4 << std::endl;
     }
     std::cout << *x << std::endl;
 
-    MySharedPtr shared_p1(new int(5));
-    std::cout << shared_p1.get_count() << std::endl;
+    std::cout << std::endl;
+
+    int* x2 = new int(5);
+    std::cout << *x2 << std::endl;
+    {
+        MySharedPtr shared_p1(x2);
+        std::cout << "Ref. Count: " << shared_p1.get_count() << std::endl;
+        {
+            MySharedPtr shared_p2(shared_p1);
+            std::cout << "Ref. Count: " << shared_p1.get_count() << std::endl;
+        }
+        std::cout << "Ref. Count: " << shared_p1.get_count() << std::endl;
+        std::cout << *x2 << std::endl;
+    }
+    std::cout << *x2 << std::endl;
 
     return EXIT_SUCCESS;
 }
